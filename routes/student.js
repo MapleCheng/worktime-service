58b24a534d291a21e9efe7504700c3e9173aebdb
MapleCheng <maplecheng1313@gmail.com>
2020-12-11 00:23:03 +0800

    add student number list

--- routes/student.js
+++ routes/student.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
+const output = require("../utils/output")["router"];
+
 const Student = require("../service/student");
 
-router.get("/number", (req, res) => {});
+router.get("/number", (req, res) => {
+  Student.studentNumberList(req, (response) => {
+    output(res, response);
+  });
+});
 
 module.exports = router;
