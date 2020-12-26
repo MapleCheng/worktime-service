c6c4e70ac8b8f269cb84bb4cd9bae9abe1290bb1
MapleCheng <maplecheng1313@gmail.com>
2020-12-26 22:39:26 +0800

    update work time

--- routes/worktime.js
+++ routes/worktime.js
@@ -5,15 +5,21 @@ const Worktime = require("../service/worktime");
 
 router
   .route("/")
-  // 新增時數
+  // 新增學生服務時數
   .post((req, res) => {
     Worktime.newWorkTime(req, (response) => {
       output(res, response);
     });
   })
-  // 取得學生時數
+  // 取得學生服務時數
   .get((req, res) => {
     Worktime.getWorkTime(req, (response) => {
+      output(res, response);
+    });
+  })
+  // 修改學生服務時數
+  .put((req, res) => {
+    Worktime.updateWorkTime(req, (response) => {
       output(res, response);
     });
   });
--- service/worktime.js
+++ service/worktime.js
@@ -20,6 +20,7 @@ module.exports = {
       const conn = sqlInfo.conn("worktime");
       let [SQLStr, SQLData, SQLFlag] = ["", [], undefined];
 
+      // 確認時段無衝突
       SQLStr =
         "SELECT \
           count(*) as size \
@@ -27,9 +28,19 @@ module.exports = {
         WHERE student_no=? \
           AND semester=? \
           AND work_date=? \
-          AND ((start_time<=? AND end_time>=?) OR (start_time<=? AND end_time>=?))";
+          AND ((start_time<=? AND end_time>=?) OR (start_time<=? AND end_time>=?) OR (start_time>=? AND end_time<=?))";
       SQLData = await Promise.resolve(
-        sqlInfo.SQLQuery(conn, SQLStr, [student_no, semester, work_date, start_time, start_time, end_time, end_time])
+        sqlInfo.SQLQuery(conn, SQLStr, [
+          student_no,
+          semester,
+          work_date,
+          start_time,
+          start_time,
+          end_time,
+          end_time,
+          start_time,
+          end_time,
+        ])
       );
 
       if (SQLData[0]["size"] !== 0) {
@@ -37,6 +48,7 @@ module.exports = {
         return;
       }
 
+      // 新增學生服務時數
       SQLStr =
         "INSERT INTO worktime(semester, student_no, work_date, start_time, end_time, finished) VALUES(?, ?, ?, ?, ?, ?)";
       SQLFlag = await Promise.resolve(
@@ -65,7 +77,7 @@ module.exports = {
       const { query } = req;
       const { semester = "", student_no = "" } = query;
 
-      // 判斷書入
+      // 判斷輸入
       if (semester === "" || student_no === "") {
         output(callback, { code: 400 });
       }
@@ -86,6 +98,83 @@ module.exports = {
       SQLData = await Promise.resolve(sqlInfo.SQLQuery(conn, SQLStr, [student_no, semester]));
 
       output(callback, { code: 200, data: [...SQLData] }, { conn });
+    } catch (err) {
+      output(callback, { code: 500 });
+      throw err;
+    }
+  },
+  updateWorkTime: async (req, callback) => {
+    try {
+      const { query, body } = req;
+      const { id = 0 } = query;
+      const { work_date = "", start_time = 0, end_time = 0 } = body;
+
+      // 判斷輸入
+      if (id === 0 || work_date === "" || start_time === 0 || end_time === 0) {
+        output(callback, { code: 400 });
+        return;
+      }
+
+      // connection SQL
+      const conn = sqlInfo.conn("worktime");
+      let [SQLStr, SQLData, SQLFlag] = ["", [], undefined];
+
+      // 取得學號、學期
+      SQLStr = "SELECT student_no, semester FROM worktime WHERE id=?";
+      SQLData = await Promise.resolve(sqlInfo.SQLQuery(conn, SQLStr, [id]));
+      if (SQLData.length !== 1) {
+        output(callback, { code: 400 }, { conn });
+        return;
+      }
+
+      const { student_no, semester } = SQLData[0];
+
+      // 確認時段無衝突
+      SQLStr =
+        "SELECT \
+          count(*) as size \
+        FROM worktime \
+        WHERE id!=? \
+          AND student_no=? \
+          AND semester=? \
+          AND work_date=? \
+          AND ((start_time<=? AND end_time>=?) OR (start_time<=? AND end_time>=?) OR (start_time>=? AND end_time<=?))";
+      SQLData = await Promise.resolve(
+        sqlInfo.SQLQuery(conn, SQLStr, [
+          id,
+          student_no,
+          semester,
+          work_date,
+          start_time,
+          start_time,
+          end_time,
+          end_time,
+          start_time,
+          end_time,
+        ])
+      );
+      if (SQLData[0]["size"] !== 0) {
+        output(callback, { code: 409 }, { conn });
+        return;
+      }
+
+      // 定義修改的學生服務時數
+      const updateQuery = {
+        work_date: moment(work_date).format("YYYY-MM-DD"),
+        start_time,
+        end_time,
+        finished: 1,
+      };
+
+      // 修改學生服務時數
+      SQLStr = "UPDATE worktime SET ? WHERE id=?";
+      SQLFlag = await Promise.resolve(sqlInfo.SQLQuery(conn, SQLStr, [updateQuery, id]));
+      if (!SQLFlag) {
+        output(callback, { code: 403 }, { conn });
+        return;
+      }
+
+      output(callback, { code: 201 }, { conn });
     } catch (err) {
       output(callback, { code: 500 });
       throw err;
