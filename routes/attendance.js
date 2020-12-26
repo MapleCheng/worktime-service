e33ca556f6326124235040fafb17c68350ac6014
MapleCheng <maplecheng1313@gmail.com>
2020-12-26 23:29:12 +0800

    init attendance

---
+++ routes/attendance.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const output = require("../utils/output")["router"];
+
+const Attendance = require("../service/attendance");
+
+module.exports = router;
---
+++ service/attendance.js
@@ -0,0 +1,8 @@
+const output = require("../utils/output")["results"];
+const sqlInfo = require("../tools/getInfo");
+const getSemester = require("../utils/getSemester");
+
+module.exports = {
+  // 取得簽到狀態
+  // 學生簽到/簽退
+};
