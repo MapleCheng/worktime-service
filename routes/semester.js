e39d74b83538d0c9437becee85c49a28c2beb631
MapleCheng <maplecheng1313@gmail.com>
2020-12-26 23:20:21 +0800

    get now semester

--- routes/semester.js
+++ routes/semester.js
@@ -3,4 +3,10 @@ const output = require("../utils/output")["router"];
 
 const Semester = require("../service/semester");
 
+router.get("/now", (req, res) => {
+  Semester.getNowSemester(req, (response) => {
+    output(res, response);
+  });
+});
+
 module.exports = router;
--- service/semester.js
+++ service/semester.js
@@ -5,4 +5,12 @@ const getSemester = require("../utils/getSemester");
 module.exports = {
   // 取得學期列表
   // 取得現在學期
+  getNowSemester: async (req, callback) => {
+    try {
+      output(callback, { code: 200, data: { semester: getSemester() } });
+    } catch (err) {
+      output(callback, { code: 500 });
+      throw err;
+    }
+  },
 };
